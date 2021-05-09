2af4418f86e7eca93c02addfa9c11f4dbd5e46fb
irisdew <ycleardew@gmail.com>
2021-05-09 21:52:27 +0900

    Resolve: filterOdd

--- Challenge/irisdew/filterOdd/solve.js
+++ Challenge/irisdew/filterOdd/solve.js
@@ -2,6 +2,6 @@ const inputArray = [4, 2, 5, 1, 3];
 
 // write your codes
 
-const answer = inputArray.filter((num) => num % 2 == 1);
+const answer = inputArray.filter((num) => num % 2 === 1);
 
 console.log(answer);
