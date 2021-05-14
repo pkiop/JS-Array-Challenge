11d8d2600de3d522a4ca492486e8a08f9ddfca14
jun <jackma914@gmail.com>
2021-05-14 18:55:47 +0900

    solve:

--- Challenge/jackma914/mapAppendOrder/solve.js
+++ Challenge/jackma914/mapAppendOrder/solve.js
@@ -1,8 +1,12 @@
 const inputArray = ["홍길동", "둘리", "루피"];
 
 // write your codes
-console.log(
-  inputArray.map((el, index) => {
-    return { name: el, order: index + 1 };
-  })
-);
+
+const result = inputArray.map((el, inx) => {
+  return {
+    name: el,
+    order: inx + 1,
+  };
+});
+
+console.log(result);
