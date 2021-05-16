fcd0123a6e49cb65df80c36d9a86b30208e2a6a5
Jaharim <jaharim1993@gmail.com>
2021-05-17 04:15:36 +0900

    solve : reduceSum, sortByPrice, sortByPriceAndQuantity

---
+++ Challenge/jaharim/reduceSum/solve.js
@@ -0,0 +1,7 @@
+const inputArray = [10, 3, 20, 5, 8, 60];
+
+// write your codes
+
+const result = inputArray.reduce((prev, curr) => prev + curr, 0);
+console.log(result);
+
---
+++ Challenge/jaharim/sortByPrice/solve.js
@@ -0,0 +1,25 @@
+const inputArray = [
+  {
+    name: '사과',
+    price: 1000,
+  },
+  {
+    name: '수박',
+    price: 5000,
+  },
+  {
+    name: '당근',
+    price: 2000,
+  },
+  {
+    name: '참외',
+    price: 10000,
+  }
+];
+
+// write your codes
+
+const result = inputArray.sort((a,b) => a.price - b.price);
+
+console.log(result);
+
