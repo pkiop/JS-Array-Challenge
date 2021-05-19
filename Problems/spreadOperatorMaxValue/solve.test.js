d9566aeb33c74b4b09111e78c4d7491761786a19
Sangshin Park <34783156+pkiop@users.noreply.github.com>
2021-05-19 23:16:18 +0900

    Update solve.test.js

--- Problems/spreadOperatorMaxValue/solve.test.js
+++ Problems/spreadOperatorMaxValue/solve.test.js
@@ -2,7 +2,7 @@ const { solution } = require('./solve');
 
 const test1 = {
   input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  answer: max : 10, min : 1,
+  answer: 'max : 10, min : 1',
 };
 
 describe('spreadOperatorMaxValue', () => {
