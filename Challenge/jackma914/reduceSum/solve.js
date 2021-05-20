be2e98e45be9145d43b7fea067031d8966496961
jun <jackma914@gmail.com>
2021-05-20 19:34:24 +0900

    sove : reduce

---
+++ Challenge/jackma914/reduceSum/solve.js
@@ -0,0 +1,16 @@
+const inputArray = [10, 3, 20, 5, 8, 60];
+
+// write your codes
+
+// # 문제제목
+
+// ## 설명
+
+// reduce 메소드를 사용해서 배열의 모든 수의 합을 구하세요.
+
+// ## Expected Output
+
+// 106
+
+const result = inputArray.reduce((acc, num) => acc + num, 0);
+console.log(result);
