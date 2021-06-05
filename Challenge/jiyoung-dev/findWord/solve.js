5d7374bce127faab48617391d6a987643ed399e7
Jiyoung <janice10508@gmail.com>
2021-06-05 20:00:26 +0900

    solve: findWord
    jest 테스팅 라이브러리 사용해서 다시실행

--- Challenge/jiyoung-dev/findWord/solve.js
+++ Challenge/jiyoung-dev/findWord/solve.js
@@ -1,6 +1,8 @@
 // write your codes
-const arr = ['잠', '자', '고', '싶', '다', '용가리']
+function solution(inputArray) {
+  return inputArray.includes('용가리');
+}
 
-console.log(arr.includes('용가리'));
+exports.solution = solution;
 
 // includes 메서드는 배열 내에 특정 요소가 포함되어 있는지 확인하여 true 또는 false를 반환한다. 
