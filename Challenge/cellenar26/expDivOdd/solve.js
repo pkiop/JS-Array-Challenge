011058a2aede91b62dbb85d7ab0f77124752ec97
cellenar26 <cellenar26@gmail.com>
2021-11-25 17:55:08 +0900

    array 복습, expDivOdd

--- Challenge/cellenar26/expDivOdd/solve.js
+++ Challenge/cellenar26/expDivOdd/solve.js
@@ -1,13 +1,16 @@
+// ## 설명
+
 // 제곱한 후 3으로 나눈 나머지가 홀수인 것 을 뽑은 배열의 총 합을 구하세요.
 function solution(inputArray) {
-    let nowArr = inputArray.filter(item => ((item*item)%3)%2 ==1);
-    let temp = 0;
-    let result = 0;
-    nowArr.forEach(element => {
-        temp = element*element;
-        result +=temp;
-    });
-    return result;
-}
+    const newArr =  inputArray.map(element => element*element);
+    
+    const fillterd = newArr.filter(item => (item%3) % 2 !== 0)
+    let sum = 0;
+    for(let i=0; i<fillterd.length; i++) {
+        sum += fillterd[i]
+    }
 
+    return sum;
+}
+solution([1, 7, 3, 4, 6]) // expected: 66
 exports.solution = solution;
