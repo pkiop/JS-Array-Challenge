09ee6fe255fc3d8ca04ddb6483dfd5c94019c241
cellenar26 <cellenar26@gmail.com>
2021-11-25 17:38:27 +0900

    everyArray를 통한 메서드 연습

--- Challenge/cellenar26/everyArray/solve.js
+++ Challenge/cellenar26/everyArray/solve.js
@@ -1,7 +1,17 @@
+// # 문제제목
+
+// ## 설명
+
+// every를 이용해서 모든 원소가 짝수인지 아닌지를 판별하세요
+
+// ## Expected Output 
+
+// true
+const inputArray = [2,4,6,8]
 function solution(inputArray) {
-    return inputArray.every((test));
+    return inputArray.every(isEven)
 }
 
-const test = (item) => item % 2 ==0;
-
+const isEven = (number) => number % 2 ===0
+console.log(solution(inputArray))
 exports.solution = solution;
