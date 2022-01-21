05372de437d7d66799eb956bb6e78bb00220786e
raccoon-ccoder <qorwjddus96@naver.com>
2022-01-21 09:19:01 +0900

    Solve : reduceNameNickname

---
+++ Challenge/jybaek96/reduceSum/solve.js
@@ -0,0 +1,5 @@
+// write your codes
+function solution(inputArray) {
+    return inputArray.reduce((pre, acc) => pre + acc, 0);
+}
+exports.solution = solution;
---
+++ Challenge/jybaek96/sortByPriceAndQuantity/solve.js
@@ -0,0 +1,11 @@
+// write your codes
+function solution(inputArray) {
+    return inputArray.sort((a,b) => {
+        if(a.price === b.price){
+            return a.quantity - b.quantity;
+        }
+        return a.price - b.price;
+    });
+}
+
+exports.solution = solution;
