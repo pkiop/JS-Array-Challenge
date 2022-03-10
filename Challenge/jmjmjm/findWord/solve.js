e52446ade58c15473b34bf921f3cda9892edc2c1
Jaeman Lim <jaeman87@gmail.com>
2022-03-11 02:30:01 +0900

    solve: findWord

--- Challenge/jmjmjm/findWord/solve.js
+++ Challenge/jmjmjm/findWord/solve.js
@@ -1,4 +1,18 @@
-// write your codes
-function solution(inputArray) {}
+// const test1 = {
+//     input: ['잠', '자', '고', '싶', '다', '용가리'],
+//     answer: true,
+// };
+// const test2 = {
+//     input: ['맛있는', '용가리치킨'],
+//     answer: false,
+// };
+// const test3 = {
+//     input: ['고질라', '용가리 ', '울트라맨'],
+//     answer: false,
+// };
 
-exports.solution = solution;
+const inputArray = ['잠', '자', '고', '싶', '다', '용가리'];
+
+let answer = inputArray.includes('용가리');
+
+console.log(answer);
