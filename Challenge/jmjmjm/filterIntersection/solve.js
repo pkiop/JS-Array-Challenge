d8d912153e3f840505f89abbdfec030dd3157f3a
Jaeman Lim <jaeman87@gmail.com>
2022-03-11 02:22:52 +0900

    solve: filterIntersection

--- Challenge/jmjmjm/filterIntersection/solve.js
+++ Challenge/jmjmjm/filterIntersection/solve.js
@@ -1,3 +1,8 @@
-function solution(inputArray1, inputArray2) {}
+const tempA = [1, 2, 3, 4, 5];
+const tempB = [3, 4, 5, 6, 7];
 
-exports.solution = solution;
+const answer = tempA.filter(e => tempB.includes(e));
+
+console.log(answer);
+
+
