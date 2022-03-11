ced5882fc7068b3ecf7735695dcabe3e03b6856c
Jaeman Lim <jaeman87@gmail.com>
2022-03-12 02:24:06 +0900

    solve: mapAppendOrder

--- Challenge/jmjmjm/mapAppendOrder/solve.js
+++ Challenge/jmjmjm/mapAppendOrder/solve.js
@@ -1,4 +1,18 @@
-// write your codes
-function solution(inputArray) {}
+// const test1 = {
+//     input: ['홍길동', '둘리', '루피'],
+//     answer: [
+//         { name: '홍길동', order: 1 },
+//         { name: '둘리', order: 2 },
+//         { name: '루피', order: 3 },
+//     ],
+// };
 
-exports.solution = solution;
+const inputArray = ['홍길동', '둘리', '루피'];
+
+const answer = inputArray.map((el, idx) => {
+    return {
+        name: el,
+        order: idx+1
+    }
+});
+
