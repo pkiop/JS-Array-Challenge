7b0ec162ecba714ddefe405431a7e3ba15b01637
Jaeman Lim <jaeman87@gmail.com>
2022-03-20 17:48:57 +0900

    solve: mapAppendOrder

--- Challenge/jmjmjm/mapAppendOrder/solve.js
+++ Challenge/jmjmjm/mapAppendOrder/solve.js
@@ -1,9 +1,8 @@
-const inputArray = ['홍길동', '둘리', '루피'];
+const inputArray = ["홍길동", "둘리", "루피"];
 
 const answer = inputArray.map((el, idx) => {
-    return {
-        name: el,
-        order: idx+1
-    }
+  return {
+    name: el,
+    order: idx + 1,
+  };
 });
-
