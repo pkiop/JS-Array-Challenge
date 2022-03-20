8c03ea5b139e16bf113971c327cb1d6fde36778b
Jaeman Lim <jaeman87@gmail.com>
2022-03-20 17:47:13 +0900

    solve: findWord

--- Challenge/jmjmjm/findWord/solve.js
+++ Challenge/jmjmjm/findWord/solve.js
@@ -1,5 +1,5 @@
-const inputArray = ['잠', '자', '고', '싶', '다', '용가리'];
+const inputArray = ["잠", "자", "고", "싶", "다", "용가리", "jmjmjm"];
 
-const answer = inputArray.includes('용가리');
+const answer = inputArray.includes("용가리");
 
 console.log(answer);
