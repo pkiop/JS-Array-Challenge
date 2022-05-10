990a7600dc1330ec43147b6e68789bc81d7b734c
suhyeon <suhyeon.kan@gmail.com>
2022-05-10 19:20:53 +0900

    Solve: filterOdd

--- Challenge/suhyeon21/filterOdd/solve.js
+++ Challenge/suhyeon21/filterOdd/solve.js
@@ -1,7 +1,6 @@
 // write your codes
-
 function solution(inputArray) {
-  return inputArray.filter((person) => person.age >= 30 && person.age < 50);
+  return inputArray.filter((num) => num % 2 === 1);
 }
 
 exports.solution = solution;
