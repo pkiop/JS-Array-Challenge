01c05cdcfbdf1d427142fe8dfe825eb3a8c41a3f
J1min <aoiharu41@gmail.com>
2023-01-19 14:44:05 +0900

    Refactor: 성능 개선

--- Challenge/J1min/expDivOdd/solve.js
+++ Challenge/J1min/expDivOdd/solve.js
@@ -1,7 +1,7 @@
 function solution(inputArray) {
   return inputArray
-    .filter((input) => input ** 2 % 3 === 1) // 조건 필터링
     .map((input) => input ** 2) // 조건에 맞게 값 변경
+    .filter((input) => input % 3 === 1) // 조건 필터링
     .reduce((total, currnt) => total + currnt); // 배열의 합
 }
 
