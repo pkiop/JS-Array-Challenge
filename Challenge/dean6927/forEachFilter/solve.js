9534c5b0cca4d86c8903567256afcd5c114d9d79
dean6927 <126789124+dean6927@users.noreply.github.com>
2023-04-19 16:19:06 +0900

    Solve: forEachFilter

---
+++ Challenge/dean6927/forEachFilter/solve.js
@@ -0,0 +1,12 @@
+// write your codes
+const inputArray = [100, 10, 20, 40];
+
+// write your codes
+
+let answer = [];
+inputArray.forEach((el) => {
+  if (el >= 40) {
+    answer.push(el);
+  }
+});
+console.log(answer);
