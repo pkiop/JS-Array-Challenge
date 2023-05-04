8c0287e2b427102d5c39ccafc2accef4a1f6f5f4
Aron <aronlee5263@gmail.com>
2023-05-04 21:50:47 +0900

    문제 해결 every 2

--- Challenge/Aronlee5263/everyArray/solve.js
+++ Challenge/Aronlee5263/everyArray/solve.js
@@ -1,7 +1,5 @@
 function solution(inputArray) {
-  const answer = inputArray.every((item) => {
-    return item % 2 === 0;
-  });
+  return inputArray.every((arr) => arr % 2 === 0);
 }
 
 exports.solution = solution;
