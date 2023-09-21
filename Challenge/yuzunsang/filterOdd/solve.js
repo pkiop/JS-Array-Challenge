3845534165d03764ef1ab9239527bf4379ee4d77
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2023-09-21 23:57:32 +0900

    filter recap & spreadOperator solved

--- Challenge/yuzunsang/filterOdd/solve.js
+++ Challenge/yuzunsang/filterOdd/solve.js
@@ -1,7 +1,8 @@
 // write your codes
 
-const solution = (inputArray) => {
-    return inputArray.filter(item => item % 2 === 1);
+function solution(inputArray) {
+    // filter는 배열에서 조건에 맞는 원소만 남겨두는 것
+    return inputArray.filter((v) => v % 2 === 1);
 }
   
 exports.solution = solution;
